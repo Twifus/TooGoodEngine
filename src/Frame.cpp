ba802cc93ad29d851444a990ed47f6901e27d1d3
ATKSkor <37736143+ATKSkor@users.noreply.github.com>
2019-09-09 21:26:11 -0400

    Update Frame.cpp

--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,12 +1,16 @@
 #include "Frame.hpp"
 #include <ctime>
+#include <cstdio>
+#include <cassert>
+#include <stdexcept>
+#include <unistd.h>
 
 namespace TooGoodEngine
 {
 	Frame::Frame()
 	{
-		lastFrame = 0;
-		deltaFrame = 0;
+		lastFrame = (double) time(NULL);
+		deltaFrame = 0.0;
 	}
 
 	double Frame::getDeltaFrame() const
@@ -16,8 +20,21 @@ namespace TooGoodEngine
 
 	void Frame::computeDeltaFrame()
 	{
-		std::clock_t t = clock();
-		deltaFrame = (double)(t - lastFrame) / CLOCKS_PER_SEC;
+		std::time_t t = time(NULL);
+		deltaFrame =(double) (t - lastFrame);
 		lastFrame = t;
 	}
+}
+
+int main() 
+{
+	using namespace TooGoodEngine;
+
+	Frame * f = new Frame();
+	f->computeDeltaFrame();
+	printf("delta frame : %f\n", f->getDeltaFrame());
+	sleep(2);
+	f->computeDeltaFrame();
+	printf("delta frame : %f", f->getDeltaFrame());
+	return EXIT_SUCCESS;
 }
