bf6dae321b2a301eec862bd4742d1fbe5c908f0d
Jean Farines <jfarines@enseirb-matmeca.fr>
2019-09-10 21:30:40 -0400

    Progress on SDL visualizer

--- tests/TestSDL.cpp
+++ tests/TestSDL.cpp
@@ -1,55 +1,108 @@
 #include <SDL.h>
-#include <iostream>
+#include "Vector3.hpp"
+#include "Particle.hpp"
 
+#define VIEW_WIDTH 1280
+#define VIEW_HEIGHT 720
 
-int main(int argc, char **argv)
+#define BOX_SIZE 50
+
+#define AXIS_WIDTH 4
+#define AXIS_OFFSET 5
+
+using namespace TooGoodEngine;
+
+class ViewPoint
 {
-	// Notre fenêtre
+public:
+	double x, y;
+	ViewPoint(double x, double y) : x(x), y(y) {}
+};
 
-	SDL_Window* fenetre(0);
-	SDL_Event evenements;
-	bool terminer(false);
+ViewPoint WorldToView(Vector3 pos)
+{
+	return ViewPoint(pos.x - BOX_SIZE / 2, VIEW_HEIGHT - pos.y - BOX_SIZE / 2);
+}
 
+void DrawAxes(SDL_Renderer *renderer)
+{
+	SDL_Rect xaxis, yaxis;
+	xaxis.w = VIEW_WIDTH - 2 * AXIS_OFFSET;
+	xaxis.h = AXIS_WIDTH;
+	xaxis.x = AXIS_OFFSET;
+	xaxis.y = VIEW_HEIGHT - 2 * AXIS_OFFSET - AXIS_WIDTH;
 
-	// Initialisation de la SDL
+	yaxis.w = AXIS_WIDTH;
+	yaxis.h = VIEW_HEIGHT - 2 * AXIS_OFFSET;
+	yaxis.x = 2 * AXIS_OFFSET;
+	yaxis.y = AXIS_OFFSET;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	// Set color to blue (will change rect color)
+	SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
+
+	SDL_RenderFillRect( renderer, &xaxis );
+	SDL_RenderFillRect( renderer, &yaxis );
+
+	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+}
+
+int main (int argc, char* argv[])
+{
+    SDL_Window* window = NULL;
+    window = SDL_CreateWindow("Simulation de tir", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+
+	if (!window)
+		return EXIT_FAILURE;
+
+    // Create a renderer
+    SDL_Renderer* renderer = NULL;
+    renderer =  SDL_CreateRenderer( window, -1, 0);
+
+	if (!renderer)
+		return EXIT_FAILURE;
+
+	// Set background to black
+    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+
+	// EVENT LOOP
+	SDL_Rect r;
+	r.w = BOX_SIZE;
+	r.h = BOX_SIZE;
+	SDL_Event event;
+	Vector3 v;
+	while (1)
 	{
-		std::cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << std::endl;
-		SDL_Quit();
+		if (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				break;
+			}
+		}
+		SDL_RenderClear(renderer);
 
-		return -1;
+		DrawAxes(renderer);
+
+		v.x++;
+		v.y++;
+		ViewPoint vp = WorldToView(v);
+
+    	r.x = vp.x;
+    	r.y = vp.y;
+
+    	// Set color to blue (will change rect color)
+    	SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+
+    	SDL_RenderFillRect( renderer, &r );
+
+		SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+
+    	SDL_RenderPresent(renderer);
 	}
 
 
-	// Création de la fenêtre
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-	fenetre = SDL_CreateWindow("Test SDL 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
-	if (fenetre == 0)
-	{
-		std::cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << std::endl;
-		SDL_Quit();
-
-		return -1;
-	}
-
-
-	// Boucle principale
-
-	while (!terminer)
-	{
-		SDL_WaitEvent(&evenements);
-
-		if (evenements.window.event == SDL_WINDOWEVENT_CLOSE)
-			terminer = true;
-	}
-
-
-	// On quitte la SDL
-
-	SDL_DestroyWindow(fenetre);
-	SDL_Quit();
-
-	return 0;
+    return EXIT_SUCCESS;
 }
