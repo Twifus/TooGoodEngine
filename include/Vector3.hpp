4b839fb7df58e9ef4761736ce73931f9016b0ec6
Baptiste Babu <baptiste.babu@bordeaux-inp.fr>
2019-09-11 00:24:10 -0400

    Documentation of vectors
    Added commentaries for all functions

--- include/Vector3.hpp
+++ include/Vector3.hpp
@@ -14,38 +14,38 @@ namespace TooGoodEngine
 		Vector3();
 		Vector3(double x, double y, double z);
 
-		bool operator==(const Vector3 & other) const;
-		bool operator!=(const Vector3 & other) const;
+		bool operator==(const Vector3 & other) const;		// Comparison, vectors are equal if they are equal component by component
+		bool operator!=(const Vector3 & other) const;		// Comparison like above
 
-		Vector3 operator+(const Vector3 & other) const;
-		Vector3 operator-(const Vector3 & other) const;
-		Vector3 operator*(const Vector3 & other) const;
-		Vector3 operator/(const Vector3 & other) const;
+		Vector3 operator+(const Vector3 & other) const;		// Addition component by component
+		Vector3 operator-(const Vector3 & other) const;		// Substraction by component
+		Vector3 operator*(const Vector3 & other) const;		// Multiplication by component
+		Vector3 operator/(const Vector3 & other) const;		// Division by component
 
-		Vector3 operator-() const;
+		Vector3 operator-() const;							// Opposite vector
 
-		Vector3& operator+=(const Vector3 & other);
-		Vector3& operator-=(const Vector3 & other);
-		Vector3& operator*=(const Vector3 & other);
-		Vector3& operator/=(const Vector3 & other);
+		Vector3& operator+=(const Vector3 & other);			// Addition by component
+		Vector3& operator-=(const Vector3 & other);			// Substraction by component
+		Vector3& operator*=(const Vector3 & other);			// Multiplication by component
+		Vector3& operator/=(const Vector3 & other);			// Division by component
 
-		Vector3& operator*=(double k);
-		Vector3& operator/=(double k);
+		Vector3& operator*=(double k);						// Multiplication vector by scalar
+		Vector3& operator/=(double k);						// Division of a vector by a scalar
 
-		double operator[](int index) const;
-		double& operator[](int index);
+		double operator[](int index) const;					// Get a component (0->x, 1->y, 2->z)
+		double& operator[](int index);						// Get a reference to a component (0->x, 1->y, 2->z)
 
-		static double Dot(const Vector3 & lhs, const Vector3 & rhs);
-		static Vector3 Cross(const Vector3 & lhs, const Vector3 & rhs);
+		static double Dot(const Vector3 & lhs, const Vector3 & rhs);	// Scalar product of two vectors
+		static Vector3 Cross(const Vector3 & lhs, const Vector3 & rhs);	// Mathematical cross product
 
-		Vector3 Normalized() const;
-		double Magnitude() const;
-		double SquaredMagnitude() const;
+		Vector3 Normalized() const;							// Returns the vector of same direction and of norm 1
+		double Magnitude() const;							// Returns the norm of the vector
+		double SquaredMagnitude() const;					// Returns the square of the norm of the vector
 	};
 
-	Vector3 operator*(const Vector3 & v, double k);
-	Vector3 operator*(double k, const Vector3 & v);
-	Vector3 operator/(const Vector3 & v, double k);
+	Vector3 operator*(const Vector3 & v, double k);			// Multiplication by scalar
+	Vector3 operator*(double k, const Vector3 & v);			// Symetrical of the multiplication by a scalar
+	Vector3 operator/(const Vector3 & v, double k);			// Division by a scalar
 
-	std::ostream& operator<<(std::ostream & stream, const Vector3 & v);
+	std::ostream& operator<<(std::ostream & stream, const Vector3 & v);	// Console display of a vector
 }
