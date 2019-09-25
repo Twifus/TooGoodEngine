976a4b08c19b4de96bffcbc195a272ebc9f0188e
Yann <yroyant@enseirb-matmeca.fr>
2019-09-25 14:56:19 -0400

    working ?

--- src/Game.cpp
+++ src/Game.cpp
@@ -31,34 +31,12 @@ void initParticles()
 		sprites.push_back(Sprite(&i));
 	}
 
-// This funtion draw white axes on the given renderer. It helps locating particles in the world space.
-void DrawAxes(SDL_Renderer *renderer)
-{
-	SDL_Rect xaxis, yaxis;
-	xaxis.w = VIEW_WIDTH - 2 * AXIS_OFFSET;
-	xaxis.h = AXIS_WIDTH;
-	xaxis.x = AXIS_OFFSET;
-	xaxis.y = VIEW_HEIGHT - 2 * AXIS_OFFSET - AXIS_WIDTH;
-
-	yaxis.w = AXIS_WIDTH;
-	yaxis.h = VIEW_HEIGHT - 2 * AXIS_OFFSET;
-	yaxis.x = 2 * AXIS_OFFSET;
-	yaxis.y = AXIS_OFFSET;
-
-	// Set color to blue (will change rect color)
-	SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
-
-	SDL_RenderFillRect( renderer, &xaxis );
-	SDL_RenderFillRect( renderer, &yaxis );
-
-	SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
 }
 
-int main (int argc, char* argv[])
+SDL_Renderer* initRenderer()
 {
 	// Create a sdl window
-    SDL_Window* window = NULL;
-    window = SDL_CreateWindow("Simulation de tir", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("Simulation de tir", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN);
 
 	if (!window)
 		return nullptr;
@@ -71,19 +49,14 @@ int main (int argc, char* argv[])
 		return nullptr;
 
 	// Set background to black
-    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 
-	SDL_Rect r;
-	r.w = BOX_SIZE;
-	r.h = BOX_SIZE;
-	SDL_Event event;
+//	r.w = BOX_SIZE;
+//	r.h = BOX_SIZE;
 	
-	// Particle init
-	Frame f = Frame();
-	std::vector<Particle> p = { Particle(), Particle(3, 0.7), Particle(5, 0.5) }; // Particle
-	std::vector<Vector3> p0 = { Vector3(0, 7, 0), Vector3(0, 5, 0), Vector3() }; // Initial pos
-	std::vector<Vector3> v = { Vector3(), Vector3(10, 0, 0), Vector3(10, 15, 0) }; // Initial velocity
-	std::vector<std::tuple<int, int, int>> c = { std::make_tuple(255, 0, 0), std::make_tuple(0, 255, 0), std::make_tuple(0, 0, 255) }; // Color
+	return renderer;
+}
+
 
 int main (int argc, char* argv[])
 {
@@ -98,7 +71,7 @@ int main (int argc, char* argv[])
 	// EVENT LOOP //
 	while (1)
 	{
-		// Measure deltaTime since last frame
+		// Mesure deltaTime since last frame
 		f.computeDeltaFrame();
 
 		// Event management
@@ -124,19 +97,7 @@ int main (int argc, char* argv[])
 		//Loop for particles display
 		for (auto &i : sprites)
 		{
-			
-			ViewPoint vp = WorldToView(p[i].position);
-			r.x = (int)vp.x;
-			r.y = (int)vp.y;
-
-			// Set color of particle
-			SDL_SetRenderDrawColor( renderer, std::get<0>(c[i]), std::get<1>(c[i]), std::get<2>(c[i]), 255 );
-
-			// Draw particle on rederer
-			SDL_RenderFillRect( renderer, &r );
-
-			// Reset color to black
-			SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+			i.Draw(renderer);
 		}
 
     	SDL_RenderPresent(renderer);
