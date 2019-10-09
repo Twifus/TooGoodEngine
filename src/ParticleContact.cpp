31bc430b76ba422dcae95262b0cf4455deb965e6
Yann <yroyant@enseirb-matmeca.fr>
2019-10-09 02:49:26 -0400

    floor fix v3 the return of the son

--- src/ParticleContact.cpp
+++ src/ParticleContact.cpp
@@ -20,7 +20,7 @@ namespace TooGoodEngine
         particleA = a;
         particleB = nullptr;
         contactNormal = planNormal.Normalized();
-        interpenetration = a->GetRadius() - Vector3::Dot(planPoint - a->position, contactNormal);
+        interpenetration = a->GetRadius() - Vector3::Dot(a->position - planPoint, contactNormal);
     }
 
 
@@ -59,7 +59,7 @@ namespace TooGoodEngine
         if (isBplane)
         {
             double mA = particleA->GetMass();
-            double uA = Vector3::Dot(particleA->GetVelocity(), contactNormal);
+            double uA = Vector3::Dot(particleA->GetVelocity(), -contactNormal);
             if (uA < 0) uA = -uA;
             particleA->Impulsion(restitution * mA * 2 * uA * contactNormal);
         }
