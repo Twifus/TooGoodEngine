20af070a8b45c21e9d0852f317750171ac782ed0
Jean Farines <moukira45@gmail.com>
2019-10-09 04:21:43 -0400

    Added tofix for CableContactGenerator

--- src/CableContactGenerator.cpp
+++ src/CableContactGenerator.cpp
@@ -8,7 +8,7 @@ namespace TooGoodEngine
 	{
 		if (CurrentLength() > maxLength)
 		{
-			contacts.push_back(ParticleContact(particles[0], particles[1], restitution));
+			contacts.push_back(ParticleContact(particles[0], particles[1], restitution)); // TOFIX
 		}
 	}
 }
