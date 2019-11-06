647f43470e15ca1912e1c5f5dfc609f3b62c7705
Jean Farines <moukira45@gmail.com>
2019-11-06 14:21:30 -0500

    Vector3 comments

--- include/Vector3.hpp
+++ include/Vector3.hpp
@@ -4,6 +4,9 @@
 
 namespace TooGoodEngine
 {
+	/**
+	 * Represents a vector in a 3D space
+	 */
 	class Vector3
 	{
 	public:
@@ -19,20 +22,20 @@ namespace TooGoodEngine
 		Vector3(double x, double y, double z);	// Construct a vector of coordinates (x, y, z)
 
 		bool operator==(const Vector3& other) const;		// Comparison, vectors are equal if they are equal component by component
-		bool operator!=(const Vector3& other) const;		// Comparison like above
+		bool operator!=(const Vector3& other) const;		// Comparison, vectors are different if at least one of their components is different
 
 		Vector3 operator-() const;							// Opposite vector
 		Vector3 operator+() const;							// Vector (just for completeness)
 
 		Vector3 operator+(const Vector3& other) const;		// Addition component by component
-		Vector3 operator-(const Vector3& other) const;		// Subtraction by component
-		Vector3 operator*(const Vector3& other) const;		// Multiplication by component
-		Vector3 operator/(const Vector3& other) const;		// Division by component
+		Vector3 operator-(const Vector3& other) const;		// Subtraction component by component
+		Vector3 operator*(const Vector3& other) const;		// Multiplication component by component
+		Vector3 operator/(const Vector3& other) const;		// Division component by component
 
-		Vector3& operator+=(const Vector3& other);			// Addition by component
-		Vector3& operator-=(const Vector3& other);			// Subtraction by component
-		Vector3& operator*=(const Vector3& other);			// Multiplication by component
-		Vector3& operator/=(const Vector3& other);			// Division by component
+		Vector3& operator+=(const Vector3& other);			// Addition component by component
+		Vector3& operator-=(const Vector3& other);			// Subtraction component by component
+		Vector3& operator*=(const Vector3& other);			// Multiplication component by component
+		Vector3& operator/=(const Vector3& other);			// Division component by component
 
 		double Dot(const Vector3& other) const;				// Scalar product of two vectors
 		Vector3 Cross(const Vector3& other) const;			// Mathematical cross product
@@ -42,12 +45,12 @@ namespace TooGoodEngine
 		double SquaredMagnitude() const;					// Returns the square of the norm of the vector
 	};
 
-	Vector3 operator*(const Vector3& v, double k);			// Multiplication by scalar
-	Vector3 operator*(double k, const Vector3& v);			// Symmetrical of the multiplication by a scalar
-	Vector3 operator/(const Vector3& v, double k);			// Division by a scalar
+	Vector3 operator*(const Vector3& v, double k);			// Multiplication of each component by a scalar
+	Vector3 operator*(double k, const Vector3& v);			// Multiplication of each component by a scalar
+	Vector3 operator/(const Vector3& v, double k);			// Division of each component by a scalar
 
-	Vector3& operator*=(Vector3& v, double k);						// Multiplication vector by scalar
-	Vector3& operator/=(Vector3& v, double k);						// Division of a vector by a scalar
+	Vector3& operator*=(Vector3& v, double k);				// Multiplication of each component by a scalar
+	Vector3& operator/=(Vector3& v, double k);				// Division of each component by a scalar
 
 	std::ostream& operator<<(std::ostream& stream, const Vector3& v);	// Console display of a vector
 }
--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -4,139 +4,152 @@
 
 namespace TooGoodEngine
 {
-	const Vector3 Vector3::zero(0, 0, 0);
-	const Vector3 Vector3::one(1, 1, 1);
-	const Vector3 Vector3::right(1, 0, 0);
-	const Vector3 Vector3::up(0, 1, 0);
-	const Vector3 Vector3::forward(0, 0, 1);
+	const Vector3 Vector3::zero(0, 0, 0);		// Vector (0, 0, 0)
+	const Vector3 Vector3::one(1, 1, 1);		// Vector (1, 1, 1)
+	const Vector3 Vector3::right(1, 0, 0);		// Vector pointing right
+	const Vector3 Vector3::up(0, 1, 0);			// Vector pointing up
+	const Vector3 Vector3::forward(0, 0, 1);	// Vector pointing forward
 
+	// Construct a vector of coordinates (0, 0, 0)
 	Vector3::Vector3() : x(0), y(0), z(0) {}
 
+	// Construct a vector of coordinates (x, y, z)
 	Vector3::Vector3(double x, double y, double z) : x(x), y(y), z(z) {}
 
+	// Comparison, vectors are equal if they are equal component by component
 	bool Vector3::operator==(const Vector3& other) const
 	{
 		return x == other.x && y == other.y && z == other.z;
 	}
 
+	// Comparison, vectors are different if at least one of their components is different
 	bool Vector3::operator!=(const Vector3& other) const
 	{
 		return !(*this == other);
 	}
 
+	// Opposite vector
 	Vector3 Vector3::operator-() const
 	{
 		return Vector3(-x, -y, -z);
 	}
 
+	// Vector (just for completeness)
 	Vector3 Vector3::operator+() const
 	{
 		return *this;
 	}
 
+	// Addition component by component
 	Vector3 Vector3::operator+(const Vector3& other) const
 	{
 		return Vector3(x + other.x, y + other.y, z + other.z);
 	}
 
+	// Subtraction component by component
 	Vector3 Vector3::operator-(const Vector3& other) const
 	{
 		return Vector3(x - other.x, y - other.y, z - other.z);
 	}
 
+	// Multiplication component by component
 	Vector3 Vector3::operator*(const Vector3& other) const
 	{
 		return Vector3(x * other.x, y * other.y, z * other.z);
 	}
 
+	// Division component by component
 	Vector3 Vector3::operator/(const Vector3& other) const
 	{
 		return Vector3(x / other.x, y / other.y, z / other.z);
 	}
 
+	// Addition component by component
 	Vector3& Vector3::operator+=(const Vector3& other)
 	{
-		x += other.x;
-		y += other.y;
-		z += other.z;
-		return *this;
+		return (*this = *this + other);
 	}
 
+	// Subtraction component by component
 	Vector3& Vector3::operator-=(const Vector3& other)
 	{
-		x -= other.x;
-		y -= other.y;
-		z -= other.z;
-		return *this;
+		return (*this = *this - other);
 	}
 
+	// Multiplication component by component
 	Vector3& Vector3::operator*=(const Vector3& other)
 	{
-		x *= other.x;
-		y *= other.y;
-		z *= other.z;
-		return *this;
+		return (*this = *this * other);
 	}
 
+	// Division component by component
 	Vector3& Vector3::operator/=(const Vector3& other)
 	{
-		x /= other.x;
-		y /= other.y;
-		z /= other.z;
-		return *this;
+		return (*this = *this / other);
 	}
 
+	// Scalar product of two vectors
 	double Vector3::Dot(const Vector3& other) const
 	{
 		return x * other.x + y * other.y + z * other.z;
 	}
 
+	// Mathematical cross product
 	Vector3 Vector3::Cross(const Vector3& other) const
 	{
 		return Vector3(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x);
 	}
 
+	// Returns the vector of same direction and of norm 1
 	Vector3 Vector3::Normalized() const
 	{
 		double magnitude = Magnitude();
 		return *this / magnitude;
 	}
 
+	// Returns the norm of the vector
 	double Vector3::Magnitude() const
 	{
 		return std::sqrt(SquaredMagnitude());
 	}
 
+	// Returns the square of the norm of the vector
 	double Vector3::SquaredMagnitude() const
 	{
 		return x * x + y * y + z * z;
 	}
 
+	// Multiplication of each component by a scalar
 	Vector3 operator*(double k, const Vector3& v)
 	{
 		return Vector3(k * v.x, k * v.y, k * v.z);
 	}
 
+	// Multiplication of each component by a scalar
 	Vector3 operator*(const Vector3& v, double k)
 	{
 		return k * v;
 	}
 
+	// Division of each component by a scalar
 	Vector3 operator/(const Vector3& v, double k)
 	{
 		return Vector3(v.x / k, v.y / k, v.z / k);
 	}
 
+	// Multiplication of each component by a scalar
 	Vector3& operator*=(Vector3& v, double k)
 	{
 		return (v = v * k);
 	}
 
+	// Division of each component by a scalar
 	Vector3& operator/=(Vector3& v, double k)
 	{
 		return (v = v * k);
 	}
 
+	// Console display of a vector
 	std::ostream& operator<<(std::ostream& out, const Vector3& v)
 	{
 		return out << "(x : " << v.x << ", y : " << v.y << ", z : " << v.z << ")";
