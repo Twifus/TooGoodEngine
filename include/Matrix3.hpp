ac013319538943bdae444a05e58c53d917987fd7
ATKSkor <37736143+ATKSkor@users.noreply.github.com>
2019-11-06 15:39:38 -0500

    Fin d'implementation des matrix4x4

--- include/Matrix3.hpp
+++ include/Matrix3.hpp
@@ -4,41 +4,42 @@
 #include <cmath>
 
 #define DATA_LENGTH 9 
+#define SIZE 3
 
 namespace TooGoodEngine {
 	class Matrix3
 	{
 	public:
-		Matrix3();
-		Matrix3(Quaternion &q);
-		Matrix3(Vector3 &vector);
-		Matrix3(double matrix[DATA_LENGTH]);
+		Matrix3();											// Construct a null matrix (0,0,0,0,0,0,0,0,0)
+		Matrix3(Quaternion &q);								// Construct a matrix from a Quaternion 
+		Matrix3(Vector3 &vector);							// Construct a matrix from a Vector (x,y,z)
+		Matrix3(double matrix[DATA_LENGTH]);				// Construct a matrix from an array of 9 double
 
-		Matrix3 operator+(const Matrix3 & matrix) const;
+		Matrix3 operator+(const Matrix3 & matrix) const;	// Addition two matrix component by component,			return a new one
 
-		Vector3 operator*(const Vector3 &vector) const;
-		Matrix3 operator*(const Matrix3 &matrix) const;
-		Matrix3 operator*(const double k) const;
+		Vector3 operator*(const Vector3 &vector) const;		// Multiplication of a matrix with a Vector				return a new Vector
+		Matrix3 operator*(const Matrix3 &matrix) const;		// Multiplication of two matrix							return a new one
+		Matrix3 operator*(const double k) const;			// Multiplication of all components of the matrix by k	return a new matrix
 
-		Matrix3 operator/(const double k) const;
+		Matrix3 operator/(const double k) const;			// Division of all components of the matrix by k		return a new matrix
 
-		double operator[](const int index) const;
-		double& operator[](const int index);
+		double operator[](const int index) const;			// Getter of the component at index index
+		double& operator[](const int index);				// Setter of the component at index index
 
-		bool operator==(const Matrix3 &matrix) const;
-		bool operator!=(const Matrix3 &matrix) const;
+		bool operator==(const Matrix3 &matrix) const;		// Comparison of the two matrix component by component
+		bool operator!=(const Matrix3 &matrix) const;		// Comparison like above
 
-		Matrix3& operator+=(const Matrix3 &matrix);
-		Matrix3& operator*=(const Matrix3 &matrix);
-		Matrix3& operator*=(const double k);
-		Matrix3& operator/=(const double k);
+		Matrix3& operator+=(const Matrix3 &matrix);			// Addition of a matrix to this							return modified matrix
+		Matrix3& operator*=(const Matrix3 &matrix);			// Multiplication of this with another matrix			return modified matrix
+		Matrix3& operator*=(const double k);				// Multiplication of this with constant k				return modified matrix
+		Matrix3& operator/=(const double k);				// Division of this with constant k						return modified matrix
 
-		double ComputeDet() const;
-		bool IsInversible() const;
-		Matrix3 Inverse() const;
-		Matrix3 Transpose() const;
-		Matrix3 SetOrientation(Quaternion &q) const;
-		Matrix3 BaseChange(Vector3 &base);
+		double ComputeDet() const;							// Computation of the determinant of this
+		bool IsInvertible() const;							// Test if a matrix is invertible
+		Matrix3 Inverse() const;							// Computation of the inverse matrix					return a new one
+		Matrix3 Transpose() const;							// Transposition of this								return a new matrix
+		void SetOrientation(Quaternion &q);					// Modification of this depending on a Quaternion		return void
+		Matrix3 BaseChange(Vector3 &base);					// Base changement of this								return a new matrix
 
 
 	private:
---
+++ include/Matrix4.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "Vector3.hpp"
+#include <cmath>
+
+#define DATA_LENGTH 12 
+#define SIZE 4
+
+namespace TooGoodEngine {
+	class Matrix4
+	{
+	public:
+		Matrix4();											// Construct a null matrix (0,0,0,0,0,0,0,0,0)
+		Matrix4(Vector3 &vector);							// Construct a matrix from a Vector (x,y,z)
+		Matrix4(double matrix[DATA_LENGTH]);				// Construct a matrix from an array of 9 double
+
+		Matrix4 operator+(const Matrix4 & matrix) const;	// Addition two matrix component by component,			return a new one
+
+		Vector3 operator*(const Vector3 &vector) const;		// Multiplication of a matrix with a Vector				return a new Vector
+		Matrix4 operator*(const Matrix4 &matrix) const;		// Multiplication of two matrix							return a new one
+		Matrix4 operator*(const double k) const;			// Multiplication of all components of the matrix by k	return a new matrix
+
+		Matrix4 operator/(const double k) const;			// Division of all components of the matrix by k		return a new matrix
+
+		double operator[](const int index) const;			// Getter of the component at index index
+		double& operator[](const int index);				// Setter of the component at index index
+
+		bool operator==(const Matrix4 &matrix) const;		// Comparison of the two matrix component by component
+		bool operator!=(const Matrix4 &matrix) const;		// Comparison like above
+
+		Matrix4& operator+=(const Matrix4 &matrix);			// Addition of a matrix to this							return modified matrix
+		Matrix4& operator*=(const Matrix4 &matrix);			// Multiplication of this with another matrix			return modified matrix
+		Matrix4& operator*=(const double k);				// Multiplication of this with constant k				return modified matrix
+		Matrix4& operator/=(const double k);				// Division of this with constant k						return modified matrix
+
+		double ComputeDet() const;							// Computation of the determinant of this
+		bool IsInvertible() const;							// Test if a matrix is invertible
+		Matrix4 Inverse() const;							// Computation of the inverse matrix					return a new one
+		Matrix4 Transpose() const;							// Transposition of this								return a new matrix
+		Matrix4 BaseChange(Vector3 &base);					// Base changement of this								return a new matrix
+
+	private:
+		double data[DATA_LENGTH];
+	};
+}
