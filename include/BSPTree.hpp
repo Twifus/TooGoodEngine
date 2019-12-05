ffc4d61b55bd62d98a407d7b1451d44af9a3bf02
Yann <yroyant@enseirb-matmeca.fr>
2019-12-05 15:42:48 -0500

    BSPTree code almost finished, only missing Collider definition

--- include/BSPTree.hpp
+++ include/BSPTree.hpp
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "Vector3.hpp"
+
+struct BSPNode;
+struct Object;
+
+class BSP {
+private:
+    list<BSPNode> root;
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> distribution;
+
+    void SplitNode(BSPNode &node);
+    void EvaluateNode(BSPNode &node);
+
+public:
+    BSP(Vector3 pos_start);
+    void AddObject(const Object &object);
+    void Evaluate();
+};
