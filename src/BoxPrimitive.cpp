9a3ba12e189a4c0aeeeb25dd1483be5817adb140
Jean Farines <moukira45@gmail.com>
2019-12-10 18:39:16 -0500

    Fixed boxprimitive half size

--- src/BoxPrimitive.cpp
+++ src/BoxPrimitive.cpp
@@ -6,7 +6,7 @@
 
 namespace TooGoodEngine
 {
-	BoxPrimitive::BoxPrimitive(RigidBody* rigidbody,  Vector3& size) : rigidBody(rigidbody), size(size) {}
+	BoxPrimitive::BoxPrimitive(RigidBody* rigidbody,  Vector3& halfSize) : rigidBody(rigidbody), halfSize(halfSize) {}
 
 	std::vector<Contacts::Contact> BoxPrimitive::ResolveCollision(const Primitive& other) const
 	{
@@ -14,14 +14,24 @@ namespace TooGoodEngine
 		{
 			std::vector<Contacts::Contact> res;
 			auto plan = reinterpret_cast<const PlanPrimitive&>(other);
-			std::array<Vector3, 8> directions{ Vector3(1, 0, 0), Vector3(-1, 0, 0), Vector3(0, 1, 0), Vector3(0, -1, 0), Vector3(0, 0, 1), Vector3(0, 0, -1) };
+			std::array<Vector3, 8> directions
+			{
+				Vector3( 1,  1,  1),
+				Vector3(-1,  1,  1),
+				Vector3( 1, -1,  1),
+				Vector3( 1,  1, -1),
+				Vector3(-1, -1,  1),
+				Vector3(-1,  1, -1),
+				Vector3( 1, -1, -1),
+				Vector3(-1, -1, -1)
+			};
 			for (Vector3 dir : directions)
 			{
-				Vector3 vertex = rigidBody->GetOrientation() * (dir * size) + rigidBody->GetPosition();
+				Vector3 vertex = rigidBody->GetOrientation() * (dir * halfSize) + rigidBody->GetPosition();
 				double dist = plan.DistanceToPlan(vertex);
 				if (dist <= 0)
 				{
-					res.emplace_back(vertex, plan.GetNormal(), dist);
+					res.emplace_back(vertex, plan.GetNormal(), std::abs(dist));
 				}
 			}
 			return res;
